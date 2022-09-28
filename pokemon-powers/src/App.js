5bf7697ed80111e8114aff762928aaf0bd39f34d
hilaryyates <hilryyates@gmail.com>
2022-09-27 20:15:36 -0400

    new button component

--- pokemon-powers/src/App.js
+++ pokemon-powers/src/App.js
@@ -1,12 +1,32 @@
+import React, { Component } from "react";
 import "./App.css";
 import SearchBox from "./components/SearchBox";
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div>
-      <SearchBox />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { searchField: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon").then((response) =>
+      response.json()
+    );
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Button />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ pokemon-powers/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <input type='button' value='Search!' />
+    </div>
+  );
+};
+
+export default Button;
