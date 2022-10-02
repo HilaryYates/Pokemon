64a65a849e77d82d9e8e1f995928677d5525d6a0
hilaryyates <hilryyates@gmail.com>
2022-10-01 21:26:29 -0400

    deleted comments

--- pokemon-powers/src/components/SearchBox.js
+++ pokemon-powers/src/components/SearchBox.js
@@ -49,7 +49,6 @@ const SearchBox = ({ searchChange, search }) => {
       {fetchOk ? (
         <div>
           <h3>Stats</h3>
-
           <p>moves</p>
           {moves.map((move) => (
             <ul>
@@ -84,63 +83,3 @@ const SearchBox = ({ searchChange, search }) => {
 };
 
 export default SearchBox;
-
-// class SearchBox extends Component {
-//   constructor() {
-//     super();
-//     this.state = { pokemonData: [] };
-//   }
-
-// render() {
-// console.log(this.props.search);
-// const searchPokemon = async (event) => {
-//   event.preventDefault();
-//   const response = await fetch(
-//     "https://pokeapi.co/api/v2/pokemon/" + this.props.search
-//   );
-//   const data = await response.json();
-//   console.log(data);
-
-// console.log(pokemonData);
-
-// this.componentDidUpdate()
-
-// this.setState({ pokemonData: data }, () => {
-// console.log(pokemonData);
-// const pokemon = this.state.data.name;
-
-// this.setState({ pokemonData: abilities }, () => {
-//   const abilities = this.state.pokemonData.abilities.map(
-//     (power) => power.ability.name
-//   );
-// });
-// const abilities = this.state.pokemonData.abilities.map(
-//   (power) => power.ability.name
-// );
-
-//   const heldItems = data.held_items.map((item) => item.item.name);
-//   const moves = data.moves.map((move) => move.move.name);
-// });
-// console.log(pokemonData);
-//     };
-//     return (
-//       <div>
-//         <h1>Pokemon</h1>
-//         <form onSubmit={searchPokemon}>
-//           <input
-//             type='search'
-//             placeholder='search pokemon'
-//             onChange={this.props.searchChange}
-//           ></input>
-//           <input type='button' value='Search!' />
-//         </form>
-//         <h2>{this.state.pokemonData.name}</h2>
-//         <h3>Stats</h3>
-//         <p>abilities</p>
-//         <p>held items</p>
-//         <p>moves</p>
-//       </div>
-//     );
-//   }
-// }
-// export default SearchBox;
